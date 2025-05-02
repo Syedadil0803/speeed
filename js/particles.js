736200cef8bdfb4282b1d7739967a6a2e77b6a95
syedadil0803 <129782085+Syedadil0803@users.noreply.github.com>
2025-05-02 12:17:12 +0530

    changed loader to 1.5 sec

--- js/particles.js
+++ js/particles.js
@@ -122,6 +122,6 @@ class ParticleCanvas {
       loader.style.opacity = '0';
       loader.style.pointerEvents = 'none';
       setTimeout(() => loader.style.display = 'none', 500);
-    }, 1000); // delay 2 seconds
+    }, 1500); // delay 1.5 seconds
   });
   
